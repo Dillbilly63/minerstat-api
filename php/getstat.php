06511c9590f39177fcf5e44b9df591f4116591e2
coinscrow <hello@coinscrow.io>
2017-07-21 12:13:04 +0200

    Update getstat.php

--- php/getstat.php
+++ php/getstat.php
@@ -1,9 +1,6 @@
-
 <?php
 
-// OUTDATED we reccomend to use the new v2 API !
-
-// API EXAMPLE PHP v1.0
+// API EXAMPLE PHP v2.0
 // CONTACT: api@minerstat.com
 
 function load($url) {
@@ -16,41 +13,58 @@ curl_close($ch);
 return $data;
 }
 
-// CHANGE IT TO YOUR WEBSITE LOGIN
-$token = "XY1234";
+$token = "CHANGEME"; // CHANGE IT TO YOUR WEBSITE LOGIN
 
-$json = load("https://minerstat.com/api/v1/getstat/$token");
+$json = load("https://minerstat.com/api/v2/getstat/$token");
 $data = json_decode($json, true);
 
 // FIX VARIBLES
-$total_online_worker=$data['0']['total_online_worker'];
-$total_worker=$data['1']['total_worker'];
-$total_gpu=$data['2']['total_gpu'];
+$total_online_worker=$data['global']['total_online_worker'];
+$total_worker=$data['global']['total_worker'];
+$total_gpu=$data['global']['total_gpu'];
 
-// ADD +3, BECAUSE THE FIRST 3 VARIBLE IS FIXED..
-$total = $total_worker + 3; 
-$x=3;
 ?>
 
-<table>
+<html>
+<head>
+<link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+
+<table style="position: fixed; margin-left: 15%; margin-top: 15%; width: 70%;"  class="pure-table">
 <tbody>
 
-<th>
+<thead>
 <tr>
-<td>WORKER</td>
+<td>RIG</td>
+<td>CRYPTO</td>
+<td>UPTIME</td>
 <td>STATUS</td>
 <td>HASHRATE</td>
+<td>SHARES</td>
+<td>POOL</td>
 </tr>
-</th>
+</thead>
 
 <?php
-while ($x != $total) {
+$x=0;
+
+while ($x != $total_worker) {
+
+$worker=strtoupper($data["list"][$x]);
+
 ?>
 
 <tr>
-<td><?php echo $data[$x]['worker'];  ?></td>
-<td><?php echo $data[$x]['status'];  ?></td>
-<td><?php echo $data[$x]['hashrate'];  ?> <?php echo $data[$x]['hashrate_unit'];  ?></td>
+<td><?php echo $data[$worker]['general']['worker'];  ?></td>
+<td><?php echo $data[$worker]['general']['crypto'];  ?></td>
+<td><?php echo $data[$worker]['general']['uptime'];  ?></td>
+<td><?php echo $data[$worker]['general']['status'];  ?></td>
+<td><?php echo $data[$worker]['hashrate']['hashrate'];  ?> <?php echo $data[$worker]['hashrate']['hashrate_unit'];  ?></td>
+<td><?php echo $data[$worker]['shares']['accepted_share'];  ?> / <?php echo $data[$worker]['shares']['accepted_share'];  ?></td>
+<td><?php echo $data[$worker]['general']['pool'];  ?></td>
 </tr>
 
 <?php
